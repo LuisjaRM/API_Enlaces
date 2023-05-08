ccf8aaf91bdae553268037c7c7378043625d8938
luisja.rodriguez <luisjaroma@gmail.com>
2023-05-08 16:30:32 +0200

    Modificaciones estructura base

--- controllers/urls/modifyUrl.js
+++ controllers/urls/modifyUrl.js
@@ -12,15 +12,15 @@ const {} = require("../../jois/schemas");
 
 // Controller ↓
 
-const login = async (req, res, next) => {
+const modifyUrl = async (req, res, next) => {
   try {
     res.send({
       status: "ok",
-      message: "Soy un post de login",
+      message: "Soy un path de modifyUrl",
     });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { login };
+module.exports = { modifyUrl };

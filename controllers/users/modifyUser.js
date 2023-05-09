b346499a0f97d8b88d16c8dc1974d01c2a51818d
Mladen44 <aleksandrovamladen0@gmail.com>
2023-05-09 11:11:24 +0200

    añadir modifyUser

--- controllers/users/modifyUser.js
+++ controllers/users/modifyUser.js
@@ -4,7 +4,7 @@ const { generateError } = require("../../services/generateError");
 
 // Requires Functions database ↓
 
-const {} = require("../../database/usersQueries/usersQueries");
+const {} = require("../../database/userDB");
 
 // Requires Jois ↓
 
@@ -14,13 +14,40 @@ const {} = require("../../jois/schemas");
 
 const modifyUser = async (req, res, next) => {
   try {
+    //Obtemeno id usuario.
+    const { id } = req.params;
+    //Obtenemo la info que queremofsfs
+    const { password, user } = req.body;
+    
+    // Joi validation
+    // Se validan los datos del user
+    const schema = modifyUserJoi;
+    const validation = schema.validate(req.body);
+    //Por si hay un error de validasion
+    if (validation.error) {
+      throw generateError(validation.error.message, 401);
+    }
+    // Buscamo el user
+    // Get user by id
+    const existingUser = await getUserById(id);
+    
+    // Si no existe el user o es imbecil
+    if (!existingUser) {
+      throw generateError(`No existe el usuario con el id:${id}`, 404);
+    }
+     
+    // Update user
+    await updateUserById(id, password, user);
+    //Respuesta http con mensaje 
+    // Res.send
     res.send({
       status: "ok",
-      message: "Soy un path de modifyUser",
+      message: `El usuario con el id:${id} se ha actualizado correctamente`,
     });
   } catch (error) {
     next(error);
   }
 };
+
 
 module.exports = { modifyUser };

3b7cb97b601c28a575560240b42faa92ece08bb4
Victorotr <123467870+Victorotr@users.noreply.github.com>
2023-05-15 14:18:19 +0200

    Update handleErrors.js

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -7,7 +7,6 @@ const chalk = require("chalk");
 const error = async (error, req, res, next) => {
   console.error(chalk.red(error));
   
-
   res.status(error.httpStatus || 500).send({
     status: "error",
     message: error.message,

6662f3fa00957d38b07f2f4bab2b90ac0a5aae96
luisja.rodriguez <luisjaroma@gmail.com>
2023-06-24 10:18:28 +0200

    Cambios en query para borrar usuario

--- database/usersQueries/deleteUserQuery.js
+++ database/usersQueries/deleteUserQuery.js
@@ -9,35 +9,164 @@ const deleteUserQuery = async (id) => {
 
     // Delete all user references in other tables
 
+    // Delete user likes
     await connection.query(
       `
-              DELETE FROM likes
-              WHERE user_id = ?
-            `,
+      DELETE FROM likes
+      WHERE user_id = ?
+      `,
       [id]
     );
 
+    // Delete user votes
     await connection.query(
       `
-              DELETE FROM votes
-              WHERE user_id = ?
-            `,
+      DELETE FROM votes
+      WHERE user_id = ?
+      `,
       [id]
     );
 
-    await connection.query(
+    // Select user comments with likes
+    const [comments] = await connection.query(
       `
-              DELETE FROM comments
-              WHERE user_id = ?
-            `,
+      SELECT c.*
+      FROM comments c
+      INNER JOIN likes l ON l.comment_id = c.id
+      WHERE c.user_id = ?
+      `,
       [id]
     );
 
+    // Delete likes of user comments
+    if (comments.length > 0) {
+      for (const comment of comments) {
+        await connection.query(
+          `
+        DELETE FROM likes
+        WHERE comment_id = ?
+      `,
+          [comment.id]
+        );
+      }
+    }
+
+    // Delete user comments
     await connection.query(
       `
-              DELETE FROM offers
-              WHERE user_id = ?
-            `,
+      DELETE FROM comments
+      WHERE user_id = ?
+      `,
+      [id]
+    );
+
+    // Delete user favorites
+    await connection.query(
+      `
+      DELETE FROM favorites
+      WHERE user_id = ?
+        `,
+      [id]
+    );
+
+    // Select user offers with votes
+    const [offersWithVotes] = await connection.query(
+      `
+        SELECT o.*
+        FROM offers o
+        INNER JOIN votes v ON v.offer_id = o.id
+        INNER JOIN users u ON o.user_id  = u.id
+        WHERE o.user_id = ?
+        GROUP BY o.id;
+  `,
+      [id]
+    );
+
+    // Delete votes of user offers
+    if (offersWithVotes.length > 0) {
+      for (const offer of offersWithVotes) {
+        await connection.query(
+          `
+          DELETE FROM votes
+          WHERE offer_id = ?
+          `,
+          [offer.id]
+        );
+      }
+    }
+
+    // Select user offers
+    const [userOffers] = await connection.query(
+      `
+        SELECT *
+        FROM offers 
+        WHERE user_id = ?
+  `,
+      [id]
+    );
+
+    // Select likes of comments of user offers
+    if (userOffers.length > 0) {
+      for (const offer of userOffers) {
+        const [commentsWithLikes] = await connection.query(
+          `
+      SELECT c.*
+      FROM comments c
+      INNER JOIN likes l ON l.comment_id = c.id
+      INNER JOIN users u ON c.user_id  = u.id
+      WHERE offer_id = ?
+      GROUP BY c.id;
+      `,
+          [offer.id]
+        );
+
+        // Delete likes of comments of user offers
+        if (commentsWithLikes.length > 0) {
+          for (const comment of commentsWithLikes) {
+            await connection.query(
+              `
+            DELETE FROM likes
+            WHERE comment_id = ?
+          `,
+              [comment.id]
+            );
+          }
+        }
+
+        const [userOffersComments] = await connection.query(
+          `
+          SELECT c.*
+          FROM comments c
+          WHERE offer_id = ?
+      `,
+          [offer.id]
+        );
+
+        for (const comment of userOffersComments) {
+          await connection.query(
+            `
+          DELETE FROM comments
+          WHERE comment_id = ?
+        `,
+            [comment.id]
+          );
+        }
+
+        await connection.query(
+          `
+        DELETE FROM favorites
+        WHERE offer_id = ?
+      `,
+          [offer.id]
+        );
+      }
+    }
+
+    await connection.query(
+      `
+        DELETE FROM offers
+        WHERE user_id = ?
+              `,
       [id]
     );
 
